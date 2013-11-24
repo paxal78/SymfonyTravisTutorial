f5842f83fd94dea213fbddb6f413a114ad7c9fd4
paxal78 <paxal78@gmail.com>
2013-11-24 23:43:24 +0100

    #1026

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -10,7 +10,7 @@ class DemoControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/dsemo/hello/Fabien');
+        $crawler = $client->request('GET', '/demos/hello/Fabien');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Hello Fabien")')->count());
     }
